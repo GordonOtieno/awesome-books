8283495e2e8cd0855c6efb73d28b41ef0c4fec66
spydaspider <spydaspider@gmail.com>
2022-08-01 12:29:28 +0000

    Added add functionality

--- awesome.js
+++ awesome.js
@@ -1,10 +1,39 @@
-const container = document.querySelector('.container');
-container.addEventListener('click', (e) => {
+
+
+const bookWrapper = document.querySelector('.book-wrapper');
+bookWrapper.addEventListener('click', (e) => {
   if (e.target.className === 'remove') {
     const removeParent = document.querySelector('.remove').parentElement;
-    container.removeChild(removeParent);
+    bookWrapper.removeChild(removeParent);
   }
 });
+
+if(localStorage != null)
+{
+   //We will display every book from our local storage.
+}
+//Display books function.
+function displayBook(book)
+{
+    const outputDiv = document.createElement('div');
+    const bookName = document.createElement('p');
+    const author = document.createElement('p');
+    const btn = document.createElement('button');
+    outputDiv.classList.add('output');
+    btn.classList.add('remove');
+    bookName.textContent = book.title;
+    author.textContent = book.author;
+    btn.textContent = 'remove';
+    outputDiv.appendChild(bookName);
+    outputDiv.appendChild(author);
+    outputDiv.appendChild(btn);
+    bookWrapper.appendChild(outputDiv);
+
+
+    
+
+}
+
 
 let submit = document.forms['form'];
 let books = [];
@@ -19,6 +48,7 @@ submit.addEventListener('submit', (e) => {
     author: author,
   };
   books.push(book);
-
+  displayBook(book);
   localStorage.setItem('books', JSON.stringify(books));
+
 });
