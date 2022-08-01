1dd06309e92fdeb7cfa3123a9c4baa1c284615a1
GordonOtieno <otienogordon95@gmail.com>
2022-08-01 15:58:27 -0400

    modify awesome.js file

--- awesome.js
+++ awesome.js
@@ -1,9 +1,37 @@
+ const bookWrapper = document.querySelector('.book-wrapper');
+ function loadData(book)
+// {
+//     const outputDiv = document.createElement('div');
+//     const bookName = document.createElement('p');
+//     const author = document.createElement('p');
+//     const btn = document.createElement('button');
+//     const br= document.createElement('br')
+//     const hr=document.createElement('hr');
+//     outputDiv.classList.add('output');
+//     btn.classList.add('remove');
+//     bookName.textContent = book.title;
+//     author.textContent = book.author;
+//     btn.textContent = 'remove';
+//     outputDiv.appendChild(bookName);
+//     outputDiv.appendChild(author);
+//     outputDiv.appendChild(btn);
+//     outputDiv.appendChild(br);
+//     outputDiv.appendChild(br);
+//     outputDiv.appendChild(hr)
+//     bookWrapper.appendChild(outputDiv);
+// }
+const decodedbooks = JSON.parse(localStorage.getItem('books'));
+for(let i=0; i<decodedbooks.length; i +=1){
+    loadData(decodedbooks[i]);
+//   console.log(decodedbooks[i])
+}
 
 
-const bookWrapper = document.querySelector('.book-wrapper');
+
+
 bookWrapper.addEventListener('click', (e) => {
   if (e.target.className === 'remove') {
-    const removeParent = document.querySelector('.remove').parentElement;
+    const removeParent = e.target.parentElement;
     bookWrapper.removeChild(removeParent);
   }
 });
@@ -13,12 +41,15 @@ if(localStorage != null)
    //We will display every book from our local storage.
 }
 //Display books function.
+
 function displayBook(book)
 {
     const outputDiv = document.createElement('div');
     const bookName = document.createElement('p');
     const author = document.createElement('p');
     const btn = document.createElement('button');
+    const br= document.createElement('br')
+    const hr=document.createElement('hr');
     outputDiv.classList.add('output');
     btn.classList.add('remove');
     bookName.textContent = book.title;
@@ -27,13 +58,11 @@ function displayBook(book)
     outputDiv.appendChild(bookName);
     outputDiv.appendChild(author);
     outputDiv.appendChild(btn);
+    outputDiv.appendChild(br);
+    outputDiv.appendChild(br);
+    outputDiv.appendChild(hr)
     bookWrapper.appendChild(outputDiv);
-
-
-    
-
 }
-
 
 let submit = document.forms['form'];
 let books = [];
