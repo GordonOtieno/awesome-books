97a57c2fbd3d986dc9135b1217460563633168b9
GordonOtieno <otienogordon95@gmail.com>
2022-08-01 13:54:02 -0400

    modify awesome.js file

--- awesome.js
+++ awesome.js
@@ -1,10 +1,24 @@
 const container = document.querySelector('.container');
 container.addEventListener('click', (e) => {
-    if(e.target.className === 'remove')
-    {
-       const removeParent = document.querySelector('.remove').parentElement;
-       container.removeChild(removeParent);
+  if (e.target.className === 'remove') {
+    const removeParent = document.querySelector('.remove').parentElement;
+    container.removeChild(removeParent);
+  }
+});
 
-    }
+let submit = document.forms['form'];
+let books = [];
 
-})
+submit.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let title = submit.querySelector('.title').value;
+  let author = submit.querySelector('.author').value;
+  let book = {
+    title: title,
+    author: author,
+  };
+  books.push(book);
+
+  localStorage.setItem('books', JSON.stringify(books));
+});
